ca8740f07513f0121bf3496993915d794c8f5349
shaqattack13 <afujita@purdue.edu>
2021-04-25 19:51:39 -0400

    simplified uart, deleted some clutter from testing

--- uart/uart.ino
+++ uart/uart.ino
@@ -22,39 +22,13 @@ BLEDis  bledis;  // device information
 BLEUart bleuart; // uart over ble
 BLEBas  blebas;  // battery
 
-int16_t data[64] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
-11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
-21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
-31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
-41, 42, 43, 44, 45, 46, 47, 48, 49, 50,
-51, 52, 53, 54, 55, 56, 57, 58, 59, 60,
-61, 62, 63, 64};
 bool BLEisConnected = false;
 
-const int16_t samples = SAMPLES_256;
-int16_t arr[samples] = {0};
-int16_t arr2[samples] = {0};
-
-uint8_t pi[10] = {3, 1, 4, 1, 5, 9, 2, 6, 5, 3};
+uint16_t numVals = 10
+uint8_t vals[10] = {3, 1, 4, 1, 5, 9, 2, 6, 5, 3};
 
 void setup()
 {
-  /*
-  unsigned int16_t index = 0;
-  for (int i = 0; i < 1500; i++) {
-    arr[i] = PPG_HR_CLEAN_01[index];
-    index = i % samples;
-  }
-  */
-  
-  ///*
-  for (int i = 0; i < samples; i++) {
-    arr[i] = PPG_HR_CLEAN_01[i];
-  }
-  for (int i = 0; i < samples; i++) {
-    arr2[i] = PPG_HR_CLEAN_03[i];
-  }
-  //*/
   Serial.begin(115200);
 
 #if CFG_DEBUG
@@ -135,176 +109,15 @@ void startAdv(void)
 void loop()
 {
   delay(1000);
-//  if (1) {
-//    uint8_t testLim = 10; // lim seems to be 61 4-dig nums
-//    uint8_t test[testLim] = {0};
-//    decComp(arr, test, 0, testLim);
-//    print_array(test, testLim * 2);
-//    delay(5000);
-//  }
 
+  // While there is a bluetooth connection, send the array values to app
   while (BLEisConnected) {
-//    uint8_t buf[14] = {0};
-//    for (int i = 0; i < 10; i++){
-//      //Serial.println(pi[i]);
-//      buf[0] = pi[i];
-//      Serial.println(buf[0]);
-//      bleuart.write(buf, 14);
-//      delay(1);
-//    }
-    bleuart.write(pi, 10);
+    bleuart.write(vals, numVals);
     Serial.println("Done");
-    delay(5000);
+    delay(5000);          // Wait 5 seconds
   }
-
-  /*
-  int16_t index = 0;
-  while (BLEisConnected && index < 1500) {
-    unsigned long startTime = millis();
-    uint8_t buf[4] = {0};
-    int space = fill_buffer(arr, buf, 1500);
-
-    bleuart.write( buf, space );
-    
-    delay(100);
-    unsigned long currentTime = millis();
-    Serial.println(currentTime - startTime);
-    index++;
-  }
-  */
-  /*
-  while (BLEisConnected) {
-    unsigned long startTime = millis();
-    uint16_t lo = 0;
-    uint8_t rawLim = 128;
-    uint16_t bufLimit = 256; // lim seems to be 61 4-dig nums
-    uint8_t buf[bufLimit] = {0};
-    uint16_t hi;
-    
-    do {
-      hi = lo + rawLim;
-      if (hi > samples) {
-        hi = samples;
-      }
-      //Serial.println(lo);
-      //Serial.println(hi);
-      //Serial.println(arr[lo]);
-      //Serial.println(arr[hi]);
-      decComp(arr, buf, lo, hi);
-      //print_array(buf, bufLimit);
-      buf[0] = 'a';
-      buf[1] = 'b';
-      bleuart.write( buf, bufLimit );
-      //Serial.println("Bruh");
-      unsigned long currentTime = millis();
-      Serial.println(currentTime - startTime);
-      delay(1);
-      lo += rawLim;
-      hi = samples;
-      print_array(buf, bufLimit); 
-    } while (hi < samples);
-
-//    lo = 0;
-//    do {
-//      hi = lo + rawLim;
-//      if (hi > samples) {
-//        hi = samples;
-//      }
-//      decComp(arr2, buf, lo, hi);
-//      //print_array(buf, hi * 2);
-//      bleuart.write( buf, bufLimit );
-//      delay(1);
-//      lo += rawLim;
-//    } while (hi < samples);
-//    unsigned long currentTime = millis();
-//    Serial.println(currentTime - startTime);
-  
-    delay(10000);
-  }
-  */
-  
-  /* // main test code
-  while (BLEisConnected) {
-    unsigned long startTime = millis();
-    uint16_t index = 0;
-    while (index < 100) {
-      uint8_t bufLimit = 300; // lim seems to be 61 4-dig nums
-      uint8_t buf[bufLimit] = {0};
-      index = fill_buffer(arr, buf, bufLimit, index);
-      bleuart.write( buf, bufLimit );
-      delay(1);
-    }
-    index = 0;
-    while (index < 100) {
-      uint8_t bufLimit = 300; // lim seems to be 61 4-dig nums, or 81 3-dig hex, 122 2-dig compressed decimals
-      uint8_t buf[bufLimit] = {0};
-      index = fill_buffer(arr2, buf, bufLimit, index);
-      bleuart.write( buf, bufLimit );
-      delay(1);
-    }
-    unsigned long currentTime = millis();
-    Serial.println(currentTime - startTime);
-    delay(10000);
-  }
-  */
-
-  /*
-  uint16_t index = 0;
-  while (BLEisConnected) {
-    unsigned long startTime = millis();
-    uint8_t bufLimit = 200; // lim seems to be 61 4-dig nums
-    uint8_t buf[bufLimit] = {0};
-    int space = fill_buffer(arr, buf, bufLimit);
-    bleuart.write( buf, space );
-    unsigned long currentTime = millis();
-    Serial.println(currentTime - startTime);
-    //index += bufLimit / 4;
-    delay(20000);
-    
-  }
-  */
-  
-  /*
-  // Forward data from HW Serial to BLEUARTm
-  while (Serial.available())
-  {
-    // Delay to wait for enough input, since we have a limited transmission buffer
-    delay(2);
-
-    
-    uint8_t buf[64];
-    //int_to_char(data, buf, 2);
-    //int count = sizeof(buf);
-    int count = Serial.readBytes(buf, sizeof(buf));
-    Serial.println(count);
-    print_array(buf, count);
-    bleuart.write( buf, count );
-    //delay(20000);
-  }
-  */
-
-  /*
-  // Forward data from HW Serial to BLEUART
-  while (BLEisConnected) {
-    int samples = SAMPLES_256;
-    uint8_t buf[samples * 4];
-    int space = fill_buffer(PPG_HR_CLEAN_01, buf, samples);
-    //int_to_char(data, buf, 2);
-    //int count = sizeof(buf);
-    //int count = Serial.readBytes(buf, sizeof(buf));
    
-    //print_array(buf, icount);
-  
-    //Serial.println("Printing to iPhone");
-    // Delay to wait for enough input, since we have a limited transmission buffer
-    delay(5000);
-    bleuart.write( buf, space );
-    //Serial.println("Finished print");
-    delay(60000);
-  }
-  */
-   
-  // Forward from BLEUART to HW Serial
+  // Forward from BLEUART to HW Serial, try to see if app
   while ( bleuart.available() )
   {
     uint8_t ch;
